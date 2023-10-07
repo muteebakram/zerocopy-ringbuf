9dbdfad7e387c1c5a97c5dc952412d703d3afd7e
neuromancer77 <sharma.gaurav2049@gmail.com>
2023-10-07 05:55:34 -0400

    Added user changes for magic buffer.
    Signed-off-by: neuromancer77 <sharma.gaurav2049@gmail.com>

--- user/ringbuf.c
+++ user/ringbuf.c
@@ -1,23 +1,19 @@
 #include "kernel/types.h"
-#include "kernel/riscv.h"
 #include "user/user.h"
 #include "user/ringbuf.h"
+
+void store(uint64 *p, uint64 v) {
+  __atomic_store_8(p, v, __ATOMIC_SEQ_CST);
+}
+uint64 load(uint64 *p) {
+  return __atomic_load_8(p, __ATOMIC_SEQ_CST);
+}
 
 int ringbuf_count = 0;
 
 const char *names[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
 
 struct user_ring_buf user_ring_bufs[MAX_USR_RINGBUF];
-
-void store(int *p, int v)
-{
-  __atomic_store_8(p, v, __ATOMIC_SEQ_CST);
-}
-
-int load(int *p)
-{
-  return __atomic_load_8(p, __ATOMIC_SEQ_CST);
-}
 
 int get_ringbuf_by_rd(int rd, struct user_ring_buf *rb)
 {
@@ -40,85 +36,122 @@ int get_ringbuf_by_rd(int rd, struct user_ring_buf *rb)
   return -1;
 }
 
-void ringbuf_start_read(int ring_desc, char *addr, int *bytes)
+void ringbuf_start_read(int ring_desc, char **addr, int *bytes)
 {
-  struct user_ring_buf rb;
-  if (get_ringbuf_by_rd(ring_desc, &rb) != 0)
+  struct user_ring_buf *rb = 0;
+  struct book* keep;
+  char *buf = 0;
+  uint64 read, write, src_addr, final_addr;
+
+  if (get_ringbuf_by_rd(ring_desc, rb) != 0)
     return;
 
-  rb = user_ring_bufs[ring_desc];
+  printf("ringbuf_start_read name: %d\n", rb->exists);
 
-  uint64 *buf = (uint64 *)rb.buf;
-  struct book *book = (struct book *)rb.book;
+  buf = (char *)rb->buf;
+  keep = (struct book*)rb->book;
 
-  uint64 read = load((int *)book->read_done);
-  uint64 write = load((int *)book->write_done);
+  read = load(&keep->read_done);
+  write = load(&keep->write_done);
 
-  // TODO Trying to write formula to get the read addr and how many bytes available.
-  read = read % (16 * PGSIZE);
-  write = write % (16 * PGSIZE);
+  src_addr = (uint64)buf;
+  final_addr = src_addr + read/(sizeof(uint64)) + read%(USR_RINGBUF_SIZE*PG_SIZE/sizeof(uint64));
+  *addr = (char *)final_addr;
+  *bytes = (write - read);
 
-  addr = (char *)(*buf + read % 16);
-  bytes = (int *)(write - read);
-  printf("buf: %c\n", addr);
-  printf("book: %d\n", bytes);
 }
-
 void ringbuf_finish_read(int ring_desc, int bytes)
 {
+  struct user_ring_buf *rb = 0;
+  char *buf = 0;
+  struct book* keep;
+  uint64 read, write, src_addr, final_addr;
+
+  if (get_ringbuf_by_rd(ring_desc, rb) != 0)
+    return;
+
+  printf("ringbuf_finish_read name: %d\n", rb->exists);
+
+  buf = (char *)rb->buf;
+  keep = (struct book*)rb->book;
+
+  read = load(&keep->read_done);
+  write = load(&keep->write_done);
+
+  if(write - read < bytes)
+    return;
+
+  src_addr = (uint64)buf;
+  final_addr = src_addr + (read+bytes)/(sizeof(uint64)) + (read+bytes)%(USR_RINGBUF_SIZE*PG_SIZE/sizeof(uint64));
+
+  if(final_addr > src_addr + USR_RINGBUF_SIZE*PG_SIZE){
+    read =- USR_RINGBUF_SIZE*PG_SIZE;
+    write =- USR_RINGBUF_SIZE*PG_SIZE;
+  }
+
+  store(&keep->read_done, read);
+  store(&keep->write_done, write);
 }
 
 void ringbuf_start_write(int ring_desc, char **addr, int *bytes)
 {
-  struct user_ring_buf rb;
-  if (get_ringbuf_by_rd(ring_desc, &rb) != 0)
+  struct user_ring_buf *rb = 0;
+  struct book* keep;
+  char *buf = 0;
+  uint64 read, write, src_addr, final_addr;
+
+  if (get_ringbuf_by_rd(ring_desc, rb) != 0)
     return;
 
-  rb = user_ring_bufs[ring_desc];
-  struct book *book = (struct book *)(rb.buf - (16 * PGSIZE));
-  printf("write buf_addr: %p\n", book->write_done);
+  printf("ringbuf_start_write name: %d\n", rb->exists);
+
+  buf = (char *)rb->buf;
+  keep = (struct book*)rb->book;
+
+  read = load(&keep->read_done);
+  write = load(&keep->write_done);
+
+  src_addr = (uint64)buf;
+  final_addr = src_addr + write/(sizeof(uint64)) + write%(USR_RINGBUF_SIZE*PG_SIZE/sizeof(uint64));
+  *addr = (char *)final_addr;
+  *bytes = MAX_SIZE - (write - read);
 }
 
 void ringbuf_finish_write(int ring_desc, int bytes)
 {
+  struct user_ring_buf *rb = 0;
+  //char *buf = 0;
+  struct book* keep;
+  uint64 read, write; //src_addr, final_addr;
+
+  if (get_ringbuf_by_rd(ring_desc, rb) != 0)
+    return;
+
+  printf("ringbuf_finish_write name: %d\n", rb->exists);
+
+  //buf = (char *)rb->buf;
+  keep = (struct book*)rb->book;
+
+  read = load(&keep->read_done);
+  write = load(&keep->write_done);
+
+  if(MAX_SIZE - (write - read) < bytes)
+    return;
+
+  //src_addr = (uint64)buf;
+  //final_addr = src_addr + (read+bytes)/(sizeof(uint64)) + (read+bytes)%(USR_RINGBUF_SIZE*PG_SIZE/sizeof(uint64));
+
+  store(&keep->write_done, write+bytes);
 }
 
 int ringbuf_open()
 {
   int rd;
-  uint64 *buf = (uint64 *)user_ring_bufs[ringbuf_count].buf;
-  if (ringbuf(names[ringbuf_count], 1, buf))
+  if (ringbuf(names[ringbuf_count], 1, user_ring_bufs[ringbuf_count].buf))
   {
     printf("Failed to open ringbuf.\n");
     return -1;
   }
-
-  /* Create and map kernel book to userspace book. */
-
-  // Create a userspace book with values initialized to zero.
-  struct book book = {.read_done = 0, .write_done = 512};
-
-  // Map the book to particular book of ther user ringbufs
-  user_ring_bufs[ringbuf_count].book = &book;
-
-  // Get the virtual page book address from the created ringbuf
-  uint64 *book_addr = (uint64 *)(*buf - (16 * PGSIZE));
-
-  // Move the userspace book contents to the virtual page book address.
-  memmove(book_addr, user_ring_bufs[ringbuf_count].book, sizeof(struct book));
-
-  // To test if the book (virtual page book) has the userspace book.
-  // struct book *book_test;
-  // book_test = (struct book *)book_addr;
-  // printf("ringbuf_open book value: %p\n", book_test->read_done);
-
-  // Now map the user ringbuf book to the book addr so that we can directly right to kernel mapped book with user_ring_buf.book
-  user_ring_bufs[ringbuf_count].book = (void *)book_addr;
-
-  /* Map the kernel sent virtual addr to the userspace ringbuf addr */
-  user_ring_bufs[ringbuf_count].buf = (void *)buf;
-  // printf("user_ring_bufs[ringbuf_count].buf value: %p\n", *(uint64 *)(user_ring_bufs[ringbuf_count].buf));
-
   rd = ringbuf_count;
   ringbuf_count++;
 
--- user/ringbuf.h
+++ user/ringbuf.h
@@ -1,10 +1,7 @@
 #define MAX_USR_RINGBUF 10
-
-struct book
-{
-  uint64 read_done;
-  uint64 write_done;
-};
+#define USR_RINGBUF_SIZE 16
+#define PG_SIZE 4096
+#define MAX_SIZE 4096*16
 
 struct user_ring_buf
 {
@@ -13,10 +10,24 @@ struct user_ring_buf
   int exists;
 };
 
+struct book {
+   uint64 read_done, write_done;
+};
+
+// void store(int *p, int v)
+// {
+//   __atomic_store_8(p, v, __ATOMIC_SEQ_CST);
+// }
+
+// int load(int *p)
+// {
+//   return __atomic_load_8(p, __ATOMIC_SEQ_CST);
+// }
+
 int ringbuf_open(void);
 int ringbuf_close(int);
 
-void ringbuf_start_read(int ring_desc, char *addr, int *bytes);
+void ringbuf_start_read(int ring_desc, char **addr, int *bytes);
 void ringbuf_finish_read(int ring_desc, int bytes);
 void ringbuf_start_write(int ring_desc, char **addr, int *bytes);
 void ringbuf_finish_write(int ring_desc, int bytes);
